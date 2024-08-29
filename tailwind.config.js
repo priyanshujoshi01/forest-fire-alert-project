49a72df42d70fbcd732dffc35cc48cc0ae20bf77
Priyanshu Joshi <111732307+priyanshujoshi01@users.noreply.github.com>
2024-08-29 19:53:40 +0530

    Add files via upload
    git commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // This should cover all your component files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
